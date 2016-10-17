1e3b95fbafbc75bfc0eb48d1d3719370457faa7e
Raymond Usbal <raymond@philippinedev.com>
2016-10-17 11:55:27 +0800

    doctype html5

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,5 +1,8 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>{{ $published->first()->latestRevision->name }} | {{ $published->first()->latestRevision->user->name }}</title>
     <link href="{{ elixir('css/blog.css') }}" rel="stylesheet">
